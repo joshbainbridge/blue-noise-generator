8dfc6e42bd0b19596e9c00a1cda3216c9542f73e
joshbainbridge <joshbainbridge@googlemail.com>
2017-01-14 13:56:58 +0000

    Added pseudo random code for white noise computation

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,55 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+uint32_t hashUint32(uint32_t input)
+{
+  input = ~input + (input << 15);
+  input = input ^ (input >> 12);
+  input = input + (input << 2);
+  input = input ^ (input >> 4);
+  input = input * 2057;
+  input = input ^ (input >> 16);
+
+  return input;
+}
+
+float bitsToFloat(uint32_t input)
+{
+  float output;
+  memcpy(&output, &input, sizeof(uint32_t));
+
+  return output;
+}
+
+float uintToFloat(uint32_t input)
+{
+  uint32_t output = (0x7F << 23) | (input >> 0x9);
+
+  return bitsToFloat(output) - 1.f;
+}
+
+uint32_t pseudoRandomUint(uint32_t input, uint32_t scramble)
+{
+  input ^= scramble;
+  input ^= input >> 17;
+  input ^= input >> 10;
+  input *= 0xb36534e5;
+  input ^= input >> 12;
+  input ^= input >> 21;
+  input *= 0x93fc4795;
+  input ^= 0xdf6e307f;
+  input ^= input >> 17;
+  input *= 1 | scramble >> 18;
+
+  return input;
+}
+
+float pseudoRandomFloat(uint32_t input, uint32_t scramble)
+{
+  return uintToFloat(pseudoRandomUint(input, scramble));
+}
+
 int main(int argc, char const *argv[])
 {
 
